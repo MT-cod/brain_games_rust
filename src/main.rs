7e0392565389f6026f67d46d601df225ef549bed
“Константин <mammmoth@list.ru>
2024-01-27 03:21:41 +0300

    erroring

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+use std::process;
 
 fn main() {
     #[derive(Debug)]
@@ -9,24 +10,27 @@ fn main() {
     }
 
     impl Game {
-        fn greeting() -> Self {
+        fn greeting() -> Game {
             println!("Welcome to the Brain Games!\nMay I have your name?");
             let name = io::stdin().lock().lines().next().unwrap().unwrap();
 
             println!("Hello, {}!", name);
             println!("\nList of available games:");
+            println!("-----------------------");
             println!("1. Brain even");
             println!("2. Brain calc");
             println!("3. Brain GCD");
             println!("4. Brain progression");
             println!("5. Brain prime");
+            println!("-----------------------");
             println!("Enter game number please:");
-            let game_num = io::stdin().lock().lines().next().unwrap().unwrap().parse::<u8>().unwrap();
 
-            Game {
-                name: name,
-                game_num: game_num,
-                games_counter: 0,
+            match io::stdin().lock().lines().next().unwrap().unwrap().parse::<u8>() {
+                Ok(game_num) => Game { name, game_num, games_counter: 0 },
+                Err(_) => {
+                    println!("Illegal game number! Bye!");
+                    process::exit(1);
+                },
             }
         }
     }
